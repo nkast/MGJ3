4bbb55b14787a31dae95cf83ccf9bb1f52cdfe07
Nick Kastellanos <nkastellanos@gmail.com>
2022-06-04 12:39:24 +0300

    wasm proj

---
+++ src/WebGLxna/Pages/Index.razor.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using Microsoft.Xna.Framework;
+using nkast.Wasm.Canvas;
+using nkast.Wasm.Canvas.WebGL;
+using WebGLxna;
+using MGJ3;
+
+namespace WebGLxna.Pages
+{
+    public partial class Index
+    {
+        Game _game;
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+
+            if (firstRender)
+            {
+                JsRuntime.InvokeAsync<object>("initRenderJS", DotNetObjectReference.Create(this));
+            }
+        }
+
+        [JSInvokable]
+        public void TickDotNet()
+        {
+            // init game
+            if (_game == null)
+            {
+                _game = new MGJ3Game();
+                _game.Run();
+            }
+
+            // run gameloop
+            _game.Tick();
+        }
+
+    }
+}
